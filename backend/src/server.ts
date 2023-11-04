2d13263f5ce8809b0d2973fa1e0e4ea9290ea1b2
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-11-04 14:09:13 +0100

    Added cors to allow api calls from frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import apiRoutes from "../routes/api";
-// import { connectDB, pool } from "../config/database";
 const app = express();
+import cors from 'cors'
 
 // ENV files config
 require("dotenv").config({ path: "./.env" });
@@ -9,6 +9,9 @@ require("dotenv").config({ path: "./.env" });
 //Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//CORS policy
+app.use(cors())
 
 //Routes setup
 app.use("/api", apiRoutes);
