255330dd782e1db72a622de583d2d139a8b9da53
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-11-30 18:31:04 +0100

    New fetch to get user location

--- frontend/src/util/api/httpRequests.ts
+++ frontend/src/util/api/httpRequests.ts
@@ -1,2 +1,14 @@
 import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
+
+export async function fetchWeatherInformation() {}
+
+export async function fetchUserLocation() {
+  try {
+    const response = await fetch("https://ipapi.co/json");
+    const location = await response.json();
+    return location;
+  } catch (error) {
+    console.error("Error fetching user location", error);
+  }
+}
