1654de098ade7ed5acc5a43a5ae8d87142a7beb2
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-12-01 19:04:08 +0100

    Added new fetch to get current weather info from open-meteo

--- frontend/src/util/api/httpRequests.ts
+++ frontend/src/util/api/httpRequests.ts
@@ -1,8 +1,23 @@
 import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient();
 
-export async function fetchWeatherInformation() {}
+// FETCH CURRENT WEATHER
+export async function fetchWeatherInformation(
+  latitude: number,
+  longtitude: number
+) {
+  try {
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longtitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,cloud_cover,wind_speed_10m&daily=&timezone=auto&forecast_days=1`
+    );
+    const weather = await response.json();
+    return weather;
+  } catch (error) {
+    console.error("Couldn't get current weather", error);
+  }
+}
 
+//FETCH USER LOCATION
 export async function fetchUserLocation() {
   try {
     const response = await fetch("https://ipapi.co/json");
