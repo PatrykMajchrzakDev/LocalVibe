d40e6e87584c4b280f41cfcaf65c8a0b7230e575
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-12-05 19:13:25 +0100

    Added new route for places

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 // ============= Import Modules =============
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./util/api/httpRequests";
 
 // ============= Import Components =============
 import RootLayout from "./pages/Root";
 import HomePage from "./components/Home";
+import PlacesPage from "./components/Places";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +14,12 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     id: "root",
     children: [{ index: true, element: <HomePage /> }],
+  },
+  {
+    path: "/places",
+    element: <RootLayout />,
+    id: "places",
+    children: [{ element: <PlacesPage /> }],
   },
 ]);
 
