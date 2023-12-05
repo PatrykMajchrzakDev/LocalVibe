889d3dcba8d4bb061a2b9b258a6f0cad2f8115f7
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-12-05 19:15:35 +0100

    Moved userLocation fetch to root component

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 import Footer from "../components/Footer";
+import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserLocation } from "../util/api/httpRequests";
+import UserLocation from "../types/UserLocation";
 const Root = () => {
+  const [userLocationInfo, setUserLocationInfo] = useState<UserLocation | null>(
+    null
+  );
+  const { data } = useQuery({
+    queryKey: ["userLocation"],
+    queryFn: fetchUserLocation,
+    staleTime: 300000,
+  });
+  useEffect(() => {
+    setUserLocationInfo(data);
+  }, [data]);
   return (
     <>
-      <MainNavigation />
+      <MainNavigation userLocationInfo={userLocationInfo} />
       <Outlet />
       <Footer />
     </>
