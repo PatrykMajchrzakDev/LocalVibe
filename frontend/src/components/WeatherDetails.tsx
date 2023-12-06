5d4e3a57f0989d680c8dd7a3af714e31ed447248
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-12-06 18:39:29 +0100

    Updated Home Page to also include weather details fetched from open-meteo. Will make UI next

--- frontend/src/components/WeatherDetails.tsx
+++ frontend/src/components/WeatherDetails.tsx
@@ -1,8 +1,32 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchUserLocation } from "../util/api/httpRequests";
-
-const WeatherDetails = () => {
-  return <div></div>;
+import { useEffect, useState } from "react";
+import { fetchWeatherDetails } from "../util/api/httpRequests";
+import UserLocation from "../types/UserLocation";
+const WeatherDetails: React.FC<{ userLocation: UserLocation | null }> = ({
+  userLocation,
+}) => {
+  const [weatherDetails, setWeatherDetails] = useState({});
+  useEffect(() => {
+    async function fetchAndSetWeatherDetails() {
+      if (
+        userLocation &&
+        userLocation.latitude != null &&
+        userLocation.longitude != null
+      ) {
+        try {
+          const fetchedWeatherDetails = await fetchWeatherDetails(
+            userLocation.latitude,
+            userLocation.longitude
+          );
+          setWeatherDetails(fetchedWeatherDetails);
+        } catch (error) {
+          console.error("Could not fetch weather details", error);
+        }
+      }
+    }
+    fetchAndSetWeatherDetails();
+  }, [userLocation]);
+  console.log(userLocation);
+  return <div>{}</div>;
 };
 
 export default WeatherDetails;
