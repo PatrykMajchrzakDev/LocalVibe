459dc52f128985fc6d9fcd37d941586f306cf020
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-08 19:19:41 +0100

    Changed styling due to bad behaviour on small screens

--- frontend/src/components/UI/CategoryCard.tsx
+++ frontend/src/components/UI/CategoryCard.tsx
@@ -13,7 +13,7 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ img, desc, src }) => {
           <i
             className={`flex items-center justify-center p-2 text-2xl text-red-500 ${img}`}
           ></i>
-          <p className="text-lg">{desc}</p>
+          <p className="text-base">{desc}</p>
         </Link>
       </li>
     </div>
@@ -22,7 +22,7 @@ const CategoryCard: React.FC<CategoryCardProps> = ({ img, desc, src }) => {
   const clickCard = src === undefined && (
     <li className="flex flex-col items-center justify-center ">
       <i className={`flex items-center justify-center p-2 text-2xl ${img}`}></i>
-      <p className="text-lg">{desc}</p>
+      <p className="text-base sm:text-lg">{desc}</p>
     </li>
   );
 
