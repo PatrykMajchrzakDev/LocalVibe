be37f41c7729e699ec273a9d58a1a4536f0c30e5
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-08 19:20:27 +0100

    Added reviews items component

--- frontend/src/components/Home/ReviewsItem.tsx
+++ frontend/src/components/Home/ReviewsItem.tsx
@@ -1,3 +1,5 @@
+import Rating from "../UI/StarRating/Rating";
+
 interface ReviewProps {
   review: {
     id: string;
@@ -15,23 +17,26 @@ const ReviewsItem: React.FC<ReviewProps> = ({ review }) => {
   const { author, title, text, rating, created_at, location, restaurant } =
     review;
   const formattedDate = formatDate(String(created_at));
-  console.log(review);
   return (
-    <div className="w-[calc(30%-16px)] flex flex-col">
-      <div>
-        <p>{author}</p>
-        <p>{title}</p>
+    <div className="w-full sm:w-[calc(50%-32px)] xl:w-[calc(33.33%-32px)] flex flex-col m-4 px-4 py-8 border-[1px] border-bordersColor shadow-md">
+      <div className="flex flex-col">
+        <div className="flex flex-wrap">
+          <i className="fa-regular fa-circle-user flex justify-center items-center text-3xl pr-3 text-red-500"></i>
+          <div>
+            <p className="font-semibold text-lg">{author}</p>
+            <p className="">Wrote a review</p>
+          </div>
+        </div>
+        <div className="flex">
+          <p className="font-bold">{title}</p>
+          <p className="pl-1 font-semibold text-teal-800">@ {restaurant}</p>
+        </div>
       </div>
-
-      <div>
-        <p>{restaurant}</p>
-        <p>{rating}</p>
-      </div>
-
+      <Rating rating={rating} />
       <div>{text}</div>
-      <div>
-        <p>{formattedDate}</p>
+      <div className="flex justify-between flex-grow items-end pt-4">
         <p>{location}</p>
+        <p>{formattedDate}</p>
       </div>
     </div>
   );
