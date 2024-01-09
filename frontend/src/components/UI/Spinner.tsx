6a361e3d691294e743eb8da71dd577c18d4acf8b
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-09 17:49:22 +0100

    Fixed TS error due to incorrect place for class declaration for external lib

--- frontend/src/components/UI/Spinner.tsx
+++ frontend/src/components/UI/Spinner.tsx
@@ -6,13 +6,12 @@ interface SpinnerProps {
 
 const Spinner: React.FC<SpinnerProps> = ({ message }) => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
+    <div className="flex flex-col justify-center items-center w-full h-full m-5">
       <Circles
         height="50"
         width="200"
         color="teal"
         ariaLabel="circles-loading"
-        className="m-5"
       />
       <p className="text-lg text-center px-2">{message}</p>
     </div>
