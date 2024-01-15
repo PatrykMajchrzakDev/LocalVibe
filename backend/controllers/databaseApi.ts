cca77b8c004e8b95afc61226d383e58cb4b5e79d
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-15 19:03:15 +0100

    Removed redundant code used for testing

--- backend/controllers/databaseApi.ts
+++ backend/controllers/databaseApi.ts
@@ -2,13 +2,6 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 const databaseApi = {
-  getRestaurants: async (req, res) => {
-    try {
-      console.log("test");
-    } catch (error) {
-      console.log(error);
-    }
-  },
   getRandomReviews: async (req, res) => {
     try {
       //Gets all reviews from the database (there is only 20)
