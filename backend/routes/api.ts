ac411b643ec547c7ad53907fe7d6ea318deb9628
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-18 18:19:41 +0100

    Dummy places json to make components

--- backend/routes/api.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import apiController from "../controllers/databaseApi";
-
-router.get("/restaurants", apiController.getRestaurants);
-router.get("/randomReviews", apiController.getRandomReviews);
-
-export default router;
