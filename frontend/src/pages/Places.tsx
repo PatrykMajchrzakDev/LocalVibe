de4d67c1d9fd65dfc8e782d8280d8534f5e23941
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-01-18 18:22:07 +0100

    Prepared page to hold info about places

--- frontend/src/pages/Places.tsx
+++ frontend/src/pages/Places.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import PlacesItem from "../components/Places/PlacesItem";
+import Place from "../types/Place";
 const Places = () => {
-  return <div>Places</div>;
+  const [placesList, setPlacesList] = useState<Place[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const req = await fetch(
+          "http://localhost:3000/places/search?find_desc=mechanik&find_loc=Poznan%2C%20Poland"
+        );
+        const data = await req.json();
+        setPlacesList(data.places);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(placesList);
+
+  return (
+    <main>
+      <div className="h-[14vh] bg-defaultGray border-[1px] border-b-bordersColor"></div>
+      <section className="flex">
+        <div className="h-[86vh] w-[15%] bg-defaultGray">Filters</div>
+        <div className="w-[45%] bg-gray-400">
+          {placesList.map((place) => (
+            <PlacesItem key={place.displayName.text} place={place} />
+          ))}
+        </div>
+        <div>MAP</div>
+      </section>
+    </main>
+  );
 };
 
 export default Places;
