19198d7c5b3364ec28f5c41ed91113da4c52a038
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-02-05 19:17:18 +0100

    Init to display items from fetch

--- frontend/src/components/Places/PlacesItem.tsx
+++ frontend/src/components/Places/PlacesItem.tsx
@@ -3,21 +3,23 @@ import Place from "../../types/Place";
 interface PlacesItemProps {
   place: Place;
 }
-
-const PlacesItem: React.FC<PlacesItemProps> = ({ place }) => {
-  const {
-    displayName,
-    formattedAddress,
-    rating,
-    regularOpeningHours,
-    photos,
-    types,
-    websiteUri,
-    userRatingCount,
-  } = place;
-  return <div>
-    
-  </div>;
+const PlacesItem: React.FC<PlacesItemProps> = () => {
+  return <div></div>;
 };
+// const PlacesItem: React.FC<PlacesItemProps> = ({ place }) => {
+//   const {
+//     displayName,
+//     formattedAddress,
+//     rating,
+//     regularOpeningHours,
+//     photos,
+//     types,
+//     websiteUri,
+//     userRatingCount,
+//   } = place;
+//   return <div>
+
+//   </div>;
+// };
 
 export default PlacesItem;
