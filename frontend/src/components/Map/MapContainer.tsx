8d427cd1db9ad28f63aa22a3fd9fb3a6913e7acd
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-02-05 18:59:32 +0100

    Moved the file and added index to track map pins

---
+++ frontend/src/components/Map/MapContainer.tsx
@@ -0,0 +1,32 @@
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import Place from "../../types/Place";
+import MapItem from "./MapItem";
+
+interface MapProps {
+  places: Place[];
+  center: {
+    lat: number;
+    lng: number;
+  };
+  zoom: number;
+}
+
+const MapContainer: React.FC<MapProps> = ({ places, center, zoom }) => {
+  return (
+    <div className="w-full h-full">
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY as string}>
+        <Map
+          center={center}
+          zoom={zoom}
+          mapId={import.meta.env.VITE_GOOGLE_API_MAP_ID as string}
+        >
+          {places.map((place, index) => (
+            <MapItem place={place} key={place.displayName.text} index={index} />
+          ))}
+        </Map>
+      </APIProvider>
+    </div>
+  );
+};
+
+export default MapContainer;
